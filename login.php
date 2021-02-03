f40a1b44b630af9ca89cc61fb05a9bc25b539ce5
kielhuplo <kielhuplo@gmail.com>
2021-02-04 00:30:55 +0800

    login dropdown

--- login.php
+++ login.php
@@ -1,41 +1,27 @@
- <head>
+<head>
  <title>My Online Store</title>
  </head>
- <style>
- body {
-			background: url(bg.jpg) no-repeat center center fixed; 
-			-webkit-background-size: cover;
-			-moz-background-size: cover;
-			-o-background-size: cover;
-			background-size: cover;
-			margin-top: 5px;
-			
-			color: white;
-			font-family: Times New Roman;
-			font-size:30px;
-			
-			text-shadow: 2px 2px 4px #000000;
-			text-align: center;
-			
-			}
-			input[type=submit] {
-			width: 20em;  height: 3em;
-			
-			}
-			
-</style>
+ 
  <body>
 <center>
  <h1>Login Page</h1>
   <form action = "checklogin.php" method= "POST">
+  
  <table>
 <tr>
-Username:
-<input type="text" name="username" required="required" placeholder="..."/><br/>
+	<td><label>Login assssssss:</label>
+	<td><select name="loginAs">
+                    <option value="admin">Admin</option>
+                    <option value="patient">Patient</option>
+					<option value="doctor">Doctor</option>
+                    </select>
+</tr>
+	<td><label>Username:</label>
+	<td><input type="text" name="username" required="required" placeholder="..."/><br/>
 </tr>
 <tr>
-Password:
-<input type="password" name="password" required="required" placeholder="..." />
+	<td><label>Password:</label>
+	<td><input type="password" name="password" required="required" placeholder="..." />
 </tr>
  </table><br>
  <input type="submit" value="Login"/><br/><br/>
