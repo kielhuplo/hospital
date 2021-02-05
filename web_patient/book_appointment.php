e79a7eba0401299b63754c8836914be30f7e6bec
kielhuplo <kielhuplo@gmail.com>
2021-02-05 22:24:15 +0800

    added book an appointment

---
+++ web_patient/book_appointment.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <title>Patient Help</title>
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/font-awesome.css">
+    <link rel="stylesheet" href="../css/templatemo-klassy-cafe.css">
+    <link rel="stylesheet" href="../css/owl-carousel.css">
+    <link rel="stylesheet" href="../css/lightbox.css">
+    <link rel="shortcut icon" type="image/png" href="../images/transparenticon.png">
+    </head>
+    <body>
+        <p> WELCOME TO BOOK APPOINTMENT</p>
+    </body>
+    </html>
